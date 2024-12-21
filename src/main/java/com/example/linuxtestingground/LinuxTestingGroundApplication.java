3074820de74dcda2dbb410c5a6184180f94ea801
Filimon-Coding <filimon.nuguse22@gmail.com>
2024-12-21 01:51:41 +0100

    lin test com

---
+++ src/main/java/com/example/linuxtestingground/LinuxTestingGroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.linuxtestingground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinuxTestingGroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinuxTestingGroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/linuxtestingground/LinuxTestingGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.linuxtestingground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxTestingGroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
